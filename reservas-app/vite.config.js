90f51be979dc7efc15b8767d08cb051aad082ac8
Angel Meier <angelmeier10@gmail.com>
2026-06-19 20:25:18 -0300

    Arreglar resolucion de firebase en el build de Vite

--- reservas-app/vite.config.js
+++ reservas-app/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/mano-a-mano-turnos/mano-a-mano-reservas/",
+  resolve: {
+    alias: {
+      firebase: path.resolve(__dirname, "node_modules/firebase"),
+    },
+  },
+  optimizeDeps: {
+    include: ["firebase/app", "firebase/firestore"],
+  },
 });
